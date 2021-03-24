a6ab06773816ca46237be1119e2d6793a3c7fc40
allthatjazzleo <pangleo1994@gmail.com>
2021-03-24 13:31:27 +0800

    add persistent peer field

--- 1click-reconfig.sh
+++ 1click-reconfig.sh
@@ -80,8 +80,9 @@ EnableStateSync()
     LASTEST_HEIGHT=$(curl -s $RPC_SERVERS/block | jq -r .result.block.header.height)
     BLOCK_HEIGHT=$((LASTEST_HEIGHT - 300))
     TRUST_HASH=$(curl -s "$RPC_SERVERS/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+    PERSISTENT_PEERS=$(curl -sS $NETWORK_JSON | jq -r ".\"$NETWORK\".persistent_peers")
     sed -i "s/^\(seeds\s*=\s*\).*\$/\1\"\"/" $CM_CONFIG
-    sed -i "s/^\(persistent_peers\s*=\s*\).*\$/\1\"$SEEDS\"/" $CM_CONFIG
+    sed -i "s/^\(persistent_peers\s*=\s*\).*\$/\1\"$PERSISTENT_PEERS\"/" $CM_CONFIG
     sed -i "s/^\(trust_height\s*=\s*\).*\$/\1$BLOCK_HEIGHT/" $CM_CONFIG
     sed -i "s/^\(trust_hash\s*=\s*\).*\$/\1\"$TRUST_HASH\"/" $CM_CONFIG
     sed -i "s/^\(enable\s*=\s*\).*\$/\1true/" $CM_CONFIG
